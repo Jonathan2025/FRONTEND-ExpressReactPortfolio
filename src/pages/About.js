ebd8312ad05a5339f5f48596f8cf606469250a44
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-03 13:12:29 -0400

    import useState and useEffect, also created state to hold the about data

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,17 @@
+// 10) import useState and useEffect from react 
+import { useState, useEffect } from "react";
+
+
+
 // 3) created the component and added simple boiler plates
 function About(props) {
+
+    // 11 create state to hold about data
+    const [about, setAbout] = useState(null);
+
+
+
+
     return <h1>About</h1>;
   }
   
