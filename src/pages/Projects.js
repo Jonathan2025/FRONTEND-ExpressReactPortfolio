b15010b921566ebdcf50173ddda483c5a6f8f00a
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-03 13:54:17 -0400

    imported useState and useEffect to Project.js and also created state to hold projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,16 @@
+// 16 import useState and useEffect from react 
+import { useState, useEffect } from "react";
+
+
+
 // 3) created the component and added simple boiler plates
 function Projects(props) {
+
+    // 17) create state to hold projects 
+    const [projects, setProjects] = useState(null)
+    
+
+
     return <h1>Projects</h1>;
   }
   
