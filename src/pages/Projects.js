1439ef6cefd0ea4c90f3c8326f9d5f3ef9eee086
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-03 13:55:48 -0400

    18 create function to make api call to get the data in projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,7 +8,20 @@ function Projects(props) {
 
     // 17) create state to hold projects 
     const [projects, setProjects] = useState(null)
-    
+
+    //18 create function to make api call to get the data
+    const getProjectsData = async () => {
+    //make api call and get response
+    const response = await fetch(props.URL + "projects");
+    // turn response into javascript object
+    const data = await response.json();
+    // set the projects state to the data
+    setProjects(data)
+    console.log(data)
+  }
+
+
+
 
 
     return <h1>Projects</h1>;
