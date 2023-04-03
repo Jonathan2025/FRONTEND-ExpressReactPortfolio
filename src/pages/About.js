6f4ecb8a9478a9d182a33d3a39ae3bfb695101b4
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-03 13:17:08 -0400

    created an asynchonous function to make an api call

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,15 @@ function About(props) {
     // 11 create state to hold about data
     const [about, setAbout] = useState(null);
 
-
+    // 12 create an asynchronous function to make an API call 
+    const getAboutData = async () => {
+        // make api call and get response
+        const response = await fetch(props.URL + "about");
+        // turn response into javascript object
+        const data = await response.json();
+        // set the about state to the data
+        setAbout(data);
+      }
 
 
     return <h1>About</h1>;
