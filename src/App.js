1725eb1d4ddd2631b30c82c446b19bbd6e3ad82b
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-03 12:54:15 -0400

    added the backend local host4000 URL for now

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ import Projects from "./pages/Projects";
 
 
 function App() {
+  // 5) URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  const URL = "http://localhost:4000/"
+
+
+
+
+
   return (
     <div className="App">
       
