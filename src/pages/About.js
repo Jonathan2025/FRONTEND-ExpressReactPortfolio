bff1ac33978ae5ca51889d46a08dde5f2bf698ff
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-04 12:50:16 -0400

    added some styling to the about info on the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -31,7 +31,7 @@ function About(props) {
         <div className="aboutInfo">
           <h2>{about.name}</h2>
           <h3>{about.email}</h3>
-          <p>{about.bio}</p>
+          <p id="aboutBio">{about.bio}</p>
           {/* 23 now we want to add in our headshot to our profile */}
           <img src={`${props.URL}/${about.headshot}`} alt={about.headshot} width="300" height="280"/>
         </div>
