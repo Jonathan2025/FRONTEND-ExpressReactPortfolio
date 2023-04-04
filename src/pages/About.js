cededcc32b4316ad474cb6f68f2d2767bc2baeac
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-04 11:52:28 -0400

    added in my profile headshot

--- src/pages/About.js
+++ src/pages/About.js
@@ -32,7 +32,7 @@ function About(props) {
           <h2>{about.name}</h2>
           <h3>{about.email}</h3>
           <p>{about.bio}</p>
-          {/* 22 now we want to add in the link to our profile */}
+          {/* 23 now we want to add in our headshot to our profile */}
           <img src={`${props.URL}/${about.headshot}`} alt={about.headshot} width="300" height="280"/>
         </div>
       )
