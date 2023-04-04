d682bec129c6feaff7a6cad0063d8ed3c26fc08a
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-04 14:23:10 -0400

    added a simple image with text overlay for the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
 // 3) created the component and added simple boiler plates
 function Home(props) {
-    return <h1>Home</h1>;
+    return(
+      <div className="homeImage">
+        <img src="https://www.coventry.ac.uk/globalassets/media/global/08-new-research-section/cfci/news/ai-and-fintech.gif" alt="tech-image"/>
+          <div id="textOverHomeImage">
+            <h2>Welcome to Jonathan's Portfolio</h2>
+          </div>
+      </div>
+    )
   }
   
   export default Home;
